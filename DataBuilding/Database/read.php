07963e9ca6814c7db5f1dbc59d5cd28a367cbd05
peytonthehuman <33160477+peytonthehuman@users.noreply.github.com>
2020-11-14 18:30:22 -0600

    Added spec to read.php

---
+++ DataBuilding/Database/read.php
@@ -0,0 +1,10 @@
+<?php
+
+	// Call repeatedly
+	// Will return 0 when it reaches the end of the CSV files
+	// Otherwise will return an array of values from CSV with named keys
+	readEntryFromCSVs() {
+		
+	}
+
+?>
